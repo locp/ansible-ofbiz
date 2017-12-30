ccc7f45542c05d3ac793a8d01c058786cd8f9cb8
Ben Dalling <b.dalling@locp.co.uk>
2017-12-30 12:14:14 +0000

    Allow a user to specify the OFBiz version and download location.

--- spec/ofbiz_spec.rb
+++ spec/ofbiz_spec.rb
@@ -2,11 +2,21 @@ require_relative 'spec_helper'
 
 describe 'ofbiz role' do
   describe user('ofbiz') do
-    it { should exist }
-    it { should belong_to_primary_group 'ofbiz' }
+    it {
+      should exist
+      should belong_to_primary_group 'ofbiz'
+    }
   end
 
   describe package('openjdk-8-jdk-headless') do
     it { should be_installed }
+  end
+
+  describe file('/opt/ofbiz/apache-ofbiz-16.11.03/LICENSE') do
+    it {
+      should be_file
+      should be_owned_by 'ofbiz'
+      should be_grouped_into 'ofbiz'
+    }
   end
 end
