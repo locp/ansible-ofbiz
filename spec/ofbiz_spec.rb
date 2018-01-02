ce8225e90ec775848bbd703ad76bd8d286ebc5d9
Ben Dalling <b.dalling@locp.co.uk>
2018-01-02 21:05:05 +0000

    Work to be seperated out.

--- spec/ofbiz_spec.rb
+++ spec/ofbiz_spec.rb
@@ -12,11 +12,18 @@ describe 'ofbiz role' do
     it { should be_installed }
   end
 
-  describe file('/opt/ofbiz/apache-ofbiz-16.11.03/LICENSE') do
+  describe file('/home/ofbiz/apache-ofbiz-16.11.03/LICENSE') do
     it {
       should be_file
       should be_owned_by 'ofbiz'
       should be_grouped_into 'ofbiz'
+    }
+  end
+
+  describe service('ofbiz') do
+    it {
+      should be_enabled
+      should be_running
     }
   end
 end
